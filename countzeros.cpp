3356f475ff467b79c0aed8c650255ac2b15390bf
Anshika Singh <99993107+anshik24@users.noreply.github.com>
2022-05-05 19:41:20 +0530

    count zeros

---
+++ countzeros.cpp
@@ -0,0 +1,15 @@
+class Solution{   
+public:
+    int countZeroes(int arr[], int n) {
+        // code here
+        int count =0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+        count ++;
+        
+}
+    
+}
+return count;
+    }
+};

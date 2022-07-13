4c3c0848b7769e5e8a8beeaf0b2c3a0db69c0741
Anshika Singh <99993107+anshik24@users.noreply.github.com>
2022-07-13 19:07:24 +0530

    Arranging the Array

---
+++ arranging the array.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void Rearrange(int arr[], int n);
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        cin>>arr[i];
+        long long j=0;
+      
+        Rearrange( arr, n);
+      
+        for (int i = 0; i < n; i++) 
+            cout << arr[i] << " "; 
+        cout << endl;  
+    }
+    return 0; 
+} // } Driver Code Ends
+
+
+
+
+void Rearrange(int arr[], int n)
+{
+    vector<int>ans;
+    for(int i=0;i<n;i++){
+        if(arr[i]<0)
+        ans.push_back(arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]>=0)
+        ans.push_back(arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        arr[i]=ans[i];
+    }
+    
+}

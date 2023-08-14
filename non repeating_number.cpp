86390dfe2b60d3918af3f9988ca5808339a653b9
Anshika Singh <99993107+anshik24@users.noreply.github.com>
2023-08-14 15:56:34 +0530

    non repeating_number.cpp

---
+++ non repeating_number.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> singleNumber(vector<int> nums) 
+    {
+        // Code here.
+        sort(nums.begin(),nums.end());
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==nums[i+1]){
+                i++;
+            }
+            else{
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(2 * n + 2);
+    	for(int i = 0; i < 2 * n + 2; i++)
+    		cin >> v[i];
+    	Solution ob;
+    	vector<int > ans = ob.singleNumber(v);
+    	for(auto i: ans)
+    		cout << i << " ";
+    	cout << "\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
